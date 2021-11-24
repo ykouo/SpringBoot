976b7ecd943adbba4ddac380e6552b5819f90128
ykouo <ykouo@naver.com>
2021-11-24 21:05:59 +0900

    2021/11/24 - sts설치후 설정/ 깃허브 연동 테스트

---
+++ Hello/src/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package hello;
+// 이클립스 = ctrl+f11
+// keymap = 인텔리J = shift+f10
+
+
+public class HelloWorld {
+	
+	// 스프링부트 연습을 위한 첫번째 테스트!
+	public static void main(String[] args) {
+		// 복사+붙여넣기 = ctrl+d
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");
+		System.out.println("헬로우월드!");		
+	}
+
+}
